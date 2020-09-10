82a32ceef3ce0ddcbbe04a30afee8d0e008c3d09
gieoon <jun.a.kagaya@gmail.com>
2020-09-11 05:33:25 +1200

    updated PHP script for consulting2 to use

--- PHP/index.php
+++ PHP/index.php
@@ -3,19 +3,23 @@
 <?php
     // ob_end_flush();
     // get the incoming POST data
-    // $post = file_get_contents('php://input') ?? $_POST;
+    $post = file_get_contents('php://input') ?? $_POST;
     
-    $post = $_POST;
+    // $post = $_POST;
     
     // var_dump($post);
     // echo "<h1>We've received your message</h1>";
     // decode the JSON data
-    // $post = json_decode($post, true);
+    $post = json_decode($post, true);
     
 
-    $name = $post['uname'];
-	$email = $post['uemail'];
-    $msg = $post['udescription'];
+    // $name = $post['uname'];
+	// $email = $post['uemail'];
+    // $msg = $post['udescription'];
+    $name = $post['name'];
+    $email = $post['email'];
+    $company = $post['company'];
+    $url = $post['url'];
     $solutions = "";
 
     
@@ -24,9 +28,9 @@
         $solutions .= $selected."</br>";
     }
 
-    $outgoing = "Inquiry at: " . date('Y-m-d H:i:s', time())  . " \n\nProject Description: " . wordwrap($msg,200) . "\n\nCLIENT NAME: " . $name . "\nCLIENT EMAIL: " . $email . "\nNEEDS HELP WITH: " . $solutions;
+    $outgoing = "Inquiry at: " . date('Y-m-d H:i:s', time())  . "\n\nCLIENT NAME: " . $name . "\nCLIENT EMAIL: " . $email . "\nNEEDS HELP WITH: " . "\nCOMPANY: " . $company . "\nURL: " . $url . "\n\nSOLUTIONS: " . $solutions;
 
-    file_put_contents( 'debug' . date('Y-m-d H:i:s', time()) . '.log', var_export( $outgoing, true));
+    // file_put_contents( 'debug' . date('Y-m-d H:i:s', time()) . '.log', var_export( $outgoing, true));
 
     // // send email
     mail("jun.a.kagaya@gmail.com","WEBBI CONSULTING | WEB ENQUIRY",$outgoing);
